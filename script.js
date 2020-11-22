6f990db442e2aaee1ddf1c316c095cb0257ecf94
Lu Hao <haolu1125@gmail.com>
2020-11-21 22:54:09 -0500

    created function

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+
+console.log("I'm connected");
+
+var finalPassowrd = [];
+
+
+// Write password to the #password input
+
+var passwordLength = prompt("How many characters do you want your password to have");
+
+passwordLength = parseInt(passwordLength);
+
+//input validation using typeof to check anything not number
+
+if (typeof passwordLength === "number") {
+  if (passwordLength  <8) {
+    alert("Password Length must be at least 8 characters.")
+  } else if (passwordLength >24) {
+    alert("Password Length must be no more than 24 characters.")
+  } else {
+  var specialConfirm = confirm("Click OK to confirm including special characters.");
+  var lowerConfirm = confirm("Click OK to confirm including lowercases.");
+  var upperConfirm = confirm("Click OK to confirm including uppercases.");
+  var numberConfirm = confirm("Click OK to confirm including numbers.");
+  }
+} else { alert("Please put a number to indicate the password length")  
+};
+
+
+var numChoice = ["1","2","3","4","5" ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -5,10 +59,16 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+ 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
